77924a65878c337b1fb3b385e05ed7ee84f06d97
DethPlague98 <tnaes98@gmail.com>
2026-04-22 23:04:13 -0500

    initial project

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -50,3 +50,20 @@ Add Customization and expand the game:
   - Use nested conditionals and logical operators to create complex outcomes.
 
 */
+const hasSupplies = false;
+const hasGuide = true;
+
+console.log("You see two dark paths: one leads into the forest and you see a sword in the distance, the other disappears under a lake and you can see it come back on the other side.");
+const choice2 = readline.question("Do you choose to go through the 'forest' or throught the 'lake'?")
+
+if (choice2 === 'lake' && hasGuide){
+  console.log("The guide waves you forward through the lake. The waves part around you, revealing the path that continues and you navigate forward.")
+} else if (choice2 === 'lake' && !hasGuide) {
+  console.log("The lake's water parts around you as you step towards the path... it seems you won't need to get wet after all as you continue.")
+} else if (choice2 === 'forest' && hasGuide) {
+  console.log("The guide's hand finds your shoulder and steers you towards the lake's path...as the water parts around you and the path you look back and see a set of red eyes staring back at you from the forest's path.")
+} else if (choice2 === 'forest' && hasSupplies && !hasGuide){
+  console.log("You start on the forest's path, and after some time a wild pack of dogs attack you on the path. You quickly throw the meat in your supplies aside and the dogs converge on it as you run quickly down the path towards the sword, hoping to be able to defend yourself.")
+} else {
+  console.log("You start down the forest path, and after an hour of walking you realize that you are not any closer to the sword than when you started. Without supplies you eventually succumb to the forest, and as you begin to lose consciousness you see a monkey with red eyes and a knife standing over you as all fades to black.")
+}
